52c9bb56139e68c6488a65f024848409b74cc261
teri934 <kulichova.t@gmail.com>
2023-08-02 16:55:12 +0200

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace convex_hull
+{
+	partial class FormConvexHull
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonGenerateRandom = new System.Windows.Forms.Button();
+			this.PictureBox = new System.Windows.Forms.PictureBox();
+			this.buttonRemove = new System.Windows.Forms.Button();
+			this.buttonClear = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// buttonGenerateRandom
+			// 
+			this.buttonGenerateRandom.Location = new System.Drawing.Point(785, 12);
+			this.buttonGenerateRandom.Name = "buttonGenerateRandom";
+			this.buttonGenerateRandom.Size = new System.Drawing.Size(155, 74);
+			this.buttonGenerateRandom.TabIndex = 0;
+			this.buttonGenerateRandom.Text = "Generate from random points";
+			this.buttonGenerateRandom.UseVisualStyleBackColor = true;
+			this.buttonGenerateRandom.Click += new System.EventHandler(this.buttonGenerateRandom_Click);
+			// 
+			// PictureBox
+			// 
+			this.PictureBox.BackColor = System.Drawing.SystemColors.Window;
+			this.PictureBox.Location = new System.Drawing.Point(12, 12);
+			this.PictureBox.Name = "PictureBox";
+			this.PictureBox.Size = new System.Drawing.Size(736, 535);
+			this.PictureBox.TabIndex = 1;
+			this.PictureBox.TabStop = false;
+			this.PictureBox.Click += new System.EventHandler(this.PictureBox_Click);
+			// 
+			// buttonRemove
+			// 
+			this.buttonRemove.Location = new System.Drawing.Point(785, 118);
+			this.buttonRemove.Name = "buttonRemove";
+			this.buttonRemove.Size = new System.Drawing.Size(155, 74);
+			this.buttonRemove.TabIndex = 3;
+			this.buttonRemove.Text = "Remove point";
+			this.buttonRemove.UseVisualStyleBackColor = true;
+			this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+			// 
+			// buttonClear
+			// 
+			this.buttonClear.Location = new System.Drawing.Point(785, 226);
+			this.buttonClear.Name = "buttonClear";
+			this.buttonClear.Size = new System.Drawing.Size(155, 74);
+			this.buttonClear.TabIndex = 4;
+			this.buttonClear.Text = "Clear canvas";
+			this.buttonClear.UseVisualStyleBackColor = true;
+			this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+			// 
+			// FormConvexHull
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(962, 559);
+			this.Controls.Add(this.buttonClear);
+			this.Controls.Add(this.buttonRemove);
+			this.Controls.Add(this.PictureBox);
+			this.Controls.Add(this.buttonGenerateRandom);
+			this.Name = "FormConvexHull";
+			this.Text = "Convex Hull";
+			((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonGenerateRandom;
+		private System.Windows.Forms.PictureBox PictureBox;
+		private System.Windows.Forms.Button buttonRemove;
+		private System.Windows.Forms.Button buttonClear;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace convex_hull
+{
+	public partial class FormConvexHull : Form
+	{
+		JarvisAlgorithm algorithm;
+		Graphics graphics;
+		const int DRAWING_SIZE = 10;
+		const int NEGATIVE = -1;
+		List<Point> points, convexHull;
+		Point pointRemove;
+		int indexRemove;
+
+		public FormConvexHull()
+		{
+			InitializeComponent();
+			graphics = PictureBox.CreateGraphics();
+		}
+
+		/// <summary>
+		/// clear canvas and delete data from previous computations
+		/// </summary>
+		private void clearCanvas()
+		{
+			algorithm = new JarvisAlgorithm(PictureBox.Location, PictureBox.Size, 0);
+			graphics.Clear(Color.White);
+			pointRemove = new Point(NEGATIVE, NEGATIVE);
+		}
+
+		/// <summary>
+		/// drawing ellipse and filling it to draw a point
+		/// </summary>
+		/// <param name="point"></param>
+		/// <param name="pen"></param>
+		/// <param name="brush"></param>
+		private void drawPoint(Point point, Pen pen, Brush brush)
+		{
+			var rect = new Rectangle(point.X - DRAWING_SIZE / 2, point.Y - DRAWING_SIZE / 2, DRAWING_SIZE, DRAWING_SIZE);
+			graphics.DrawEllipse(pen, rect);
+			graphics.FillEllipse(brush, rect);
+		}
+
+		/// <summary>
+		/// draw all points and their convex hull
+		/// </summary>
+		private void drawConvexHull()
+		{
+			convexHull = algorithm.GetConvexHull;
+			points = algorithm.GetPoints;
+
+			for (int i = 0; i < convexHull.Count; i++)
+				graphics.DrawLine(Pens.Red, convexHull[i].X, convexHull[i].Y, convexHull[(i + 1) % convexHull.Count].X, convexHull[(i + 1) % convexHull.Count].Y);
+			for (int i = 0; i < points.Count; i++)
+			{
+				drawPoint(points[i], Pens.Black, Brushes.Black);
+			}
+		}
+
+		/// <summary>
+		/// generate random points, calculate the convex hull
+		/// and draw the result
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void buttonGenerateRandom_Click(object sender, EventArgs e)
+		{
+			clearCanvas();
+			algorithm.CreateRandomPoints();
+			algorithm.CalculateFromPoints();
+
+			drawConvexHull();
+		}
+
+		/// <summary>
+		/// if clicking on a point, recolor it and
+		/// color previous clicked pint to original black color
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void PictureBox_Click(object sender, EventArgs e) 
+		{
+			Console.WriteLine(string.Format("X: {0} Y: {1}", ((MouseEventArgs)e).X, ((MouseEventArgs)e).Y));
+			for (int i = 0; i < points.Count; i++)
+			{
+				if (((MouseEventArgs)e).X >= points[i].X - DRAWING_SIZE / 2 && ((MouseEventArgs)e).X <= points[i].X + DRAWING_SIZE / 2
+					&& ((MouseEventArgs)e).Y >= points[i].Y - DRAWING_SIZE / 2 && ((MouseEventArgs)e).Y <= points[i].Y + DRAWING_SIZE / 2)
+				{
+					if (pointRemove.X >= 0 && pointRemove.Y >= 0)
+						drawPoint(pointRemove, Pens.Black, Brushes.Black);
+					pointRemove = points[i];
+					indexRemove = i;
+					drawPoint(pointRemove, Pens.Blue, Brushes.Blue);
+				}
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void buttonClear_Click(object sender, EventArgs e)
+		{
+			clearCanvas();
+		}
+
+		private void buttonRemove_Click(object sender, EventArgs e)
+		{
+			if (!convexHull.Contains(pointRemove))
+			{
+				points.Remove(pointRemove);
+				drawPoint(pointRemove, Pens.White, Brushes.White);
+				pointRemove = new Point(NEGATIVE, NEGATIVE);
+			}
+		}
+	}
+
+	public class JarvisAlgorithm
+	{
+		Random random = new Random();
+		private readonly int minWindowX, minWindowY, maxWindowX, maxWindowY;
+
+		private List<Point> points = new List<Point>();
+		private List<Point> convexHullPoints = new List<Point>();
+
+		public JarvisAlgorithm(Point location, Size size, int drawingSize)
+		{
+			minWindowX = 0 + drawingSize;
+			minWindowY = 0 + drawingSize;
+			maxWindowX = minWindowX + size.Width - drawingSize;
+			maxWindowY = minWindowY + size.Height - drawingSize;
+		}
+
+		enum Orientation
+		{
+			CW,
+			CCW,
+			collinear
+		}
+
+		public void CreateRandomPoints()
+		{
+			for (int i = 0; i < 25; i++)
+			{
+				int x = random.Next(minWindowX, maxWindowX);
+				int y = random.Next(minWindowY, maxWindowY);
+
+				points.Add(new Point(x, y));
+			}
+		}
+
+		/// <summary>
+		/// performs orientation test for ordered triplet of points in the order like in the function parameters
+		/// </summary>
+		/// <param name="lastConvexPoint"></param>
+		/// <param name="currentPoint"></param>
+		/// <param name="testPoint"></param>
+		/// <returns>value of orientation test: 0 ~ collinear, -1 ~ CCW, 1 ~ CW</returns>
+		private Orientation orientationTest(Point lastConvexPoint, Point currentPoint, Point testPoint)
+		{
+			int value = (currentPoint.Y - lastConvexPoint.Y) * (testPoint.X - currentPoint.X) - 
+				(currentPoint.X - lastConvexPoint.X) * (testPoint.Y - currentPoint.Y);
+
+			if (value == 0)
+				return Orientation.collinear;
+			else if (value < 0)
+				return Orientation.CCW;
+			else
+				return Orientation.CW;
+		}
+
+		/// <summary>
+		/// finding the point from the set
+		/// with the lowest X coordinate
+		/// </summary>
+		/// <returns></returns>
+		private int findLeftMostPoint()
+		{
+			int leftMostIndex = 0;
+			for (int i = 1; i < points.Count; i++)
+			{
+				if (points[i].X < points[leftMostIndex].X)
+					leftMostIndex = i;
+			}
+
+			return leftMostIndex;
+		}
+
+		/// <summary>
+		/// perform Jarvis March algorithm
+		/// </summary>
+		public void CalculateFromPoints()
+		{
+			int leftMostIndex = findLeftMostPoint();
+			int lastConvexIndex = leftMostIndex;
+			int currentIndex;
+
+			do
+			{
+				convexHullPoints.Add(points[lastConvexIndex]);
+				currentIndex = (lastConvexIndex + 1) % points.Count;
+				for (int test_index = 0; test_index < points.Count; test_index++)
+				{
+					if (orientationTest(points[lastConvexIndex], points[currentIndex], points[test_index]) == Orientation.CCW)
+						currentIndex = test_index;
+				}
+				lastConvexIndex = currentIndex;
+			} while (leftMostIndex != lastConvexIndex);
+		}
+
+		public List<Point> GetConvexHull => convexHullPoints;
+		public List<Point> GetPoints => points;
+	}
+}
+
